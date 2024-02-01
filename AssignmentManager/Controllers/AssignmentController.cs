4b6135027c595408585fad8b3057652e25ec6fde
MarcelloAffonso <mars1803@gmail.com>
2024-02-01 02:06:50 -0300

    Create of the first assignment View using mock values at the Controller.

---
+++ AssignmentManager/Controllers/AssignmentController.cs
@@ -0,0 +1,37 @@
+﻿using AssignmentManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentManager.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var x = new Assignment();
+            x.AssignmentId = 1;
+            x.Status = Data.Enum.Status.Open;
+            x.Priority = Data.Enum.Priority.Trivial;
+            x.Description = "Test assignment";
+            x.LastUpdate = DateTime.Now;
+            x.Name = "Test assignment";
+
+            var notes = new List<Note>();
+
+            notes.Add(new Note()
+            {
+                NoteId = 1,
+                Description = "note 1"
+            });
+
+            notes.Add(new Note()
+            {
+                NoteId = 2,
+                Description = "note 2"
+            });
+
+            x.Notes = notes;
+
+            return View(x);
+        }
+    }
+}
