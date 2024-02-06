2a6368c789c6c05b89280425b25fc7486bd328e6
MarcelloAffonso <mars1803@gmail.com>
2024-02-06 02:05:32 -0300

    Creation of a helper to Assignments

---
+++ AssignmentManager/Helper/AssignmentHelper.cs
@@ -0,0 +1,22 @@
+﻿using AssignmentManager.Data.Enum;
+using AssignmentManager.Models;
+
+namespace AssignmentManager.Helper
+{
+    /// <summary>
+    /// Helper class to generate all Assignment objects
+    /// </summary>
+    public static class AssignmentHelper
+    {
+        public static Assignment GetNewAssignment(string name, string description, Priority priority, Status status)
+        {
+            return new Assignment
+            {
+                Name = name,
+                Description = description,
+                Status = status,
+                Priority = priority
+            };
+        }
+    }
+}
